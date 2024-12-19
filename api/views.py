720a0b4df54c80624054e036aa1b8a7d99fb7277
Victor Wambersie <victor.wambersie@ynov.com>
2024-12-19 17:37:57 +0100

    ajouts des views

--- api/views.py
+++ api/views.py
@@ -2,10 +2,3 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Concession, Vehicule
 from .serializers import ConcessionSerializer, VehiculeSerializer
 
-class ConcessionViewSet(ModelViewSet):
-    queryset = Concession.objects.all()
-    serializer_class = ConcessionSerializer
-
-class VehiculeViewSet(ModelViewSet):
-    queryset = Vehicule.objects.all()
-    serializer_class = VehiculeSerializer
